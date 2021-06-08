f2e4ae57ff88253f638224a4838f5effe922c41c
fernando-utiyama <fernandoutiyama@gmail.com>
2021-06-07 23:29:04 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
-);
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
