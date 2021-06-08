7defa9c8f2eb03b074aa5efbf0d84a00e7027e58
fernando-utiyama <fernandoutiyama@gmail.com>
2021-06-08 20:34:32 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,7 @@ import { Component } from '@angular/core';
   <footer class="footer">
   <div class="container">
   <div class="content has-text-centered">
-    <p>
-      UNIVESP - <a href="http://univesp-campinas-pi5-ui.herokuapp.com/">EngLab</a>
+    <p> <img src='/assets/logo-univesp.png'><a href="http://univesp-campinas-pi5-ui.herokuapp.com/">EngLabs</a>
     </p>
   </div>
   </div>
